c38678d7e30f38467b0417c2030318cd32e0b5d6
Rizky Ramadhan <71491992+Kyraongithub@users.noreply.github.com>
2021-07-25 15:14:13 +0700

    image

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <Layout pageTitle="Homepage">
+      <Image src="/profile.jpg" width={200} height={200} alt="profile" />
       <h1 className={styles.title}>welcome, kyra!</h1>
     </Layout>
   );
