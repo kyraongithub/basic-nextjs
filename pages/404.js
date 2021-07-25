ec6e8ec05750a12be42e9291489bec1996613f15
Rizky Ramadhan <71491992+Kyraongithub@users.noreply.github.com>
2021-07-25 14:44:19 +0700

    custom 404

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="title-not-found">
+      <h1>Ooops........</h1>
+      <h1>Halaman yang anda cari tidak ditemukan</h1>
+    </div>
+  );
+}
