e32cb29147756445687cd5f98be09efd156fbf67
Rizky Ramadhan <71491992+Kyraongithub@users.noreply.github.com>
2021-07-25 14:51:43 +0700

    auto redirect
    creating auto redirect after 2 second user access not found page, this redirect good for user bcs user does not stuck at 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  }, []);
   return (
     <div className="title-not-found">
       <h1>Ooops........</h1>
